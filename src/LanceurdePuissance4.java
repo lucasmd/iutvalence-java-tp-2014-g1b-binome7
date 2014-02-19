3c3ef23eb4b7e128fbe5135c37f2b68f8a673bba
mirandlu <mirandlu@pc-b101-08.iut-valence.fr>
2014-02-19 16:59:43 +0100

    Création des constantes emplacementvide/jaune/rouge et début de la création de la grille

--- src/LanceurdePuissance4.java
+++ src/LanceurdePuissance4.java
@@ -1,20 +1,21 @@
 import fr.iutvalence.java.tp.Puissance4.*;
 
 /**
- * @author mirandlu
  * création de la classe afin de démarrer une partie de Puissance4
+ * 
+ * @author mirandlu
  */
 public class LanceurdePuissance4
 {
-/**
- * déclaration de la classe main
- */
-public static void main (String[] args)
+	/**
+	 * déclaration de la méthode main
+	 */
+	public static void main(String[] args)
 	{
-	System.out.println("Création de la partie");
-	Puissance4 partiedePuissance4 = new Puissance4();
-	System.out.println("Démarrage de la partie");
+		System.out.println("Création de la partie");
+		Puissance4 partiedePuissance4 = new Puissance4();
+		System.out.println("Démarrage de la partie");
 		partiedePuissance4.jouer();
-	System.out.println("Fin de la partie");
+		System.out.println("Fin de la partie");
 	}
 }
--- src/Puissance4.java
+++ src/Puissance4.java
@@ -1,13 +1,31 @@
-
 public class Puissance4
 {
-
-	public void jouer()
+	public final static int emplacementvide = 0;
+	public final static int emplacementJaune = 1;
+	public final static int emplacementRouge = 2;
+			
+	/**
+	 * Création d'une nouvelle partie, avec 2 nouveaux joueurs et une grille est vide.
+	 */
+	public Puissance4()
 	{
-		// TODO Auto-generated method stub
+		public grille(int[7],[6])
 		
+		{
+			
+		}
+		new joueur()
+		{
+			
+		}
 	}
 
+	/**
+	 * Jouer la partie
+	 */
+	public void jouer()
+	{
 
+	}
 
 }
