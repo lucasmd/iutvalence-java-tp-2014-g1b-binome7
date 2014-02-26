daaf335a4d57794de1e72487bb8d2630e12ce50b
mirandlu <mirandlu@pc-b101-03.iut-valence.fr>
2014-02-26 15:23:22 +0100

    J'ai déplacer les classes dans un paquetage

---
+++ src/fr/iutvalence/java/tp/Puissance4/LanceurdePuissance4.java
@@ -0,0 +1,27 @@
+package fr.iutvalence.java.tp.Puissance4;
+
+// TODO (fait) déplacer la classe dans un paquetage
+
+
+// TODO(fait) écrire un commentaire précis
+/**
+ * La classe qui représente le menu de démarrage de la partie de Puissance4
+ * 
+ * @author mirandlu
+ */
+public class LanceurdePuissance4
+{
+	// TODO (fait) écrire un commentaire précis
+	/**
+	 * déclaration de la méthode main permettant de créer la partie, et de la démarrer 
+	 * une sorte de "menu"
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Création de la partie");
+		Puissance4 partiedePuissance4 = new Puissance4();
+		System.out.println("Démarrage de la partie");
+		partiedePuissance4.jouer();
+		System.out.println("Fin de la partie");
+	}
+}
---
+++ src/fr/iutvalence/java/tp/Puissance4/Puissance4.java
@@ -0,0 +1,57 @@
+package fr.iutvalence.java.tp.Puissance4;
+
+// TODO (fait) écrire un commentaire 
+/**
+ * @author mirandlu
+ *	La classe qui représente une partie (prete a jouer, ou en cours de jeu)
+ */
+public class Puissance4
+{
+	/**
+	 * Déclaration de la constante pour l'emplacement vide
+	 */
+	// TODO (fait) écrire un commentaire
+	// TODO (fait) respecter les conventions d'écriture
+	public final static int EMPLACEMENTVIDE = 0;
+
+	/**
+	 * Déclaration de la constante pour l'emplacement d'un pion Jaune
+	 */
+	// TODO (fait) écrire un commentaire
+	// TODO (fait)  respecter les conventions d'écriture
+	public final static int EMPLACEMENTJAUNE = 1;
+	
+	/**
+	 * Déclaration de la constante pour l'emplacement d'un pion Rouge
+	 */
+	// TODO (fait)  écrire un commentaire
+	// TODO (fait) respecter les conventions d'écriture
+	public final static int EMPLACEMENTROUGE = 2;
+			
+	/**
+	 * Création d'une nouvelle partie, avec 2 nouveaux joueurs et une grille est vide.
+	 */
+	public Puissance4()
+	{
+		
+		// TODO ???
+		public grille(int[7],[6])
+		
+		{
+			
+		}
+		new joueur()
+		{
+			
+		}
+	}
+
+	/**
+	 * Jouer la partie
+	 */
+	public void jouer()
+	{
+
+	}
+
+}
