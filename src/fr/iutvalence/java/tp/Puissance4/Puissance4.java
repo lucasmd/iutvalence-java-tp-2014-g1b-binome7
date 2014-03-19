f17a6f5ed3dda682604cface622917ce611b8cdd
mirandlu <mirandlu@pc-b102-03.iut-valence.fr>
2014-03-19 15:26:00 +0100

    Création des procédures joueurSuivant, caseLibre getColone et amélioration de la procédure jouer avec les attributs joueuractuel et resultat

--- src/fr/iutvalence/java/tp/Puissance4/Puissance4.java
+++ src/fr/iutvalence/java/tp/Puissance4/Puissance4.java
@@ -22,7 +22,17 @@ public class Puissance4
 	 * Création d'un attribut joueur2 de type Joueur
 	 */
 	private Joueur joueur2;
-			
+
+	/**
+	 * Création d'un attribut joueur2 de type Joueur
+	 */
+	private Joueur joueuractuel;
+	
+	/**
+	 * Création d'un attribut pour le résultat
+	 */
+	private Puissance4 resultat;
+	
 	/**
 	 * Création d'une nouvelle partie, avec 2 nouveaux joueurs et une grille  vide.
 	 */
@@ -41,21 +51,50 @@ public class Puissance4
 	}
 
 	
-
+	/**
+	 *  Création de la procédure permettant de passer au joueur suivant
+	 */
+	private void joueurSuivant () 
+	{
+	if (this.joueuractuel == this.joueur1)
+	this.joueuractuel = this.joueur2 ;
+	else
+	this.joueuractuel = this.joueur1 ;
+	
+	/**
+	 * Création de la fonction permettant de savoir si une une case est libre
+	 */
+	private boolean caseLibre (int colones) 
+	{
+		return grille[colones][lignes] == Emplacement.VIDE ;
+	}
+	
+	/**
+	 * Création de la procédure getColone pour savoir dans quelle colone jouer
+	 */
+	private void getColone (int colones) {
+		if (caseLibre == Emplacement.VIDE)
+		System.out.println( " La colonne est pleine " ) ;
+		else
+		//On affecte a cette meme colone la nouvelle valeur ?
+		
+	}
+	
 
 	/**
 	 * Jouer la partie
 	 */
-/*	public void jouer()
+	public void jouer()
 	{
 		 while (NbPionsAllignes<4)
-			 if (joueur1 a joué)
-			 	joueur2 joue
-			 else joueur1 joue*/
-		 /* Afin de jouer il faudrait faire un getColone() ou le joueur dise le numéro
-		  * de colone ou il veut placer son pion */
+			 if (this.joueuractuel = joueur1)
+			 	getColone()
+			 else 
+				 joueurSuivant()
+			if (resultat == Emplacement.VIDE)
+				System.out.println( " La partie est nulle (égalité) " ) ;
+			else
+				System.out.println(this.joueuractuel + " gagne " ) ;
 	
 		
-
-	}
-
+}
